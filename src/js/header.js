68c7823a0a1200eb9fcbf7fcdb1166e2425ce87c
Sergei <s.leushkin1509@gmail.com>
2025-07-06 15:41:05 +0300

    fixed some colors

--- src/js/header.js
+++ src/js/header.js
@@ -1,32 +1,29 @@
-// let darkMode = false;
-// document.getElementById('themeToggle').addEventListener('click', () => {
-//   darkMode = !darkMode;
-//   if (darkMode) {
-//     document.documentElement.style.setProperty('--main--test--color', '#FFF');
-//     document.documentElement.style.setProperty('--main--bg--color', '#1e252b');
-//   } else {
-//     document.documentElement.style.setProperty(
-//       '--main--test--color',
-//       '#000000'
-//     );
-//     document.documentElement.style.setProperty('--main--bg--color', '#FFF');
-//   };
-// })
-
 let darkMode = false;
 document.getElementById('themeToggle').addEventListener('click', () => {
   darkMode = !darkMode;
   if (darkMode) {
-    document.querySelector('.main-container-bg-color').style.backgroundColor =
-      '#1e252b';
-    document.body.style.color = '#FFF';
-    document.querySelector('.header').style.backgroundColor = '#1e252b';
-    document.querySelector('.footer').style.backgroundColor = '#1e252b';
+    document.documentElement.style.setProperty('--main--test--color', '#FFF');
+    document.documentElement.style.setProperty('--main--bg--color', '#1e252b');
   } else {
-    document.querySelector('.main-container-bg-color').style.backgroundColor =
-      '#FFF';
-    document.body.style.color = '#000000';
-    document.querySelector('.header').style.backgroundColor = '#FFF';
-    document.querySelector('.footer').style.backgroundColor = '#FFF';
-  }
+    document.documentElement.style.setProperty('--main--test--color','#000000');
+    document.documentElement.style.setProperty('--main--bg--color', '#FFF');
+  };
 })
+
+// let darkMode = false;
+// document.getElementById('themeToggle').addEventListener('click', () => {
+//   darkMode = !darkMode;
+//   if (darkMode) {
+//     document.querySelector('.main-container-bg-color').style.backgroundColor =
+//       '#1e252b';
+//     document.body.style.color = '#FFF';
+//     document.querySelector('.header').style.backgroundColor = '#1e252b';
+//     document.querySelector('.footer').style.backgroundColor = '#1e252b';
+//   } else {
+//     document.querySelector('.main-container-bg-color').style.backgroundColor =
+//       '#FFF';
+//     document.body.style.color = '#000000';
+//     document.querySelector('.header').style.backgroundColor = '#FFF';
+//     document.querySelector('.footer').style.backgroundColor = '#FFF';
+//   }
+// })
