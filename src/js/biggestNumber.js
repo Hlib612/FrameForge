65a6dfdbcb04fbb377e32f3e71ca59e47326d403
Kira <gajdukkira232@gmail.com>
2025-07-12 10:22:56 +0300

    up

--- src/js/biggestNumber.js
+++ src/js/biggestNumber.js
@@ -1,13 +1,1 @@
 
-const inputs = document.querySelectorAll('.biggest-number-inputs');
-const result = document.getElementById('result');
-
-inputs.forEach(input => {
-  input.addEventListener('input', () => {
-    const values = Array.from(inputs).map(el => Number(el.value));
-    if (values.every(v => !isNaN(v))) {
-      const max = Math.max(...values);
-      result.textContent = `Найбільше число, яке ви ввели – ${max}`;
-    }
-  });
-});
